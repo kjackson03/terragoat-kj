37053d736adf7ee363ffff0d4e30600c263c4216
Kevin Jackson <kjackson03@gmail.com>
2024-01-16 16:53:54 -0600

    added test tag

--- terraform/aws/ec2.tf
+++ terraform/aws/ec2.tf
@@ -28,6 +28,7 @@ EOF
     git_org              = "bridgecrewio"
     git_repo             = "terragoat"
     yor_trace            = "347af3cd-4f70-4632-aca3-4d5e30ffc0b6"
+    test                 = "prismaCloud"
   })
 }
 
